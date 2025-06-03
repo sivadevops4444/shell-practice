f543dda3c89a05080bd1ee6bf53693c3506a9524
siva <sivadevops4444@gmail.com>
2025-06-03 16:38:59 +0530

    test created

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "siva : Hi viswa how are you ?"
+echo "viswa : Hi siva i am fine. How are you ?"
+echo "siva : I am good"
+echo "viswa: what are you doing"
+echo "siva : Nothing just reading and practicing the DevOps course and what are you doing"
+echo "viswa : I am also doing good and doing practice Linux course"
+echo "siva : Good to here that. Keep doing"
+
+echo "viswa : Ok then Bye. Take care"
+echo "siva : Ok Bye"
