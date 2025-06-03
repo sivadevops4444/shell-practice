b8f64d32e2c5ee887e6f69430deab4bf87913d22
siva <sivadevops4444@gmail.com>
2025-06-03 17:17:12 +0530

    siva

---
+++ 02-variable.sh
@@ -0,0 +1,14 @@
+PERSON1=$1
+PERSON2=$2
+
+
+
+echo "$PERSON1 : Hi $PERSON2, How are you"
+echo "$PERSON2 : HI $PERSON1, I am good and How about you ?"
+echo "$PERSON1 : I am also good"
+echo "$PERSON2 : OK BYE"
+
+echo " The conversation has been ended. Thank you for the statememt"
+
+
+
