e3ced24abe305241f685b99e55b2439b98970245
siva <sivadevops4444@gmail.com>
2025-06-03 17:05:46 +0530

    siva variable

---
+++ siva-variable.sh
@@ -0,0 +1,12 @@
+PERSON1=SIVA
+PERSON2=VISWA
+
+
+echo "$PERSON1 : Hey $PERSON2, How are you"
+echo "$PERSON2 : Hi $PERSON1, I am good How are you"
+echo "$PERSON1 : Yah i am also good. What are you doing"
+echo "$PERSON2 : Nothing, Just doing practice"
+echo "$PERSON1 : OK BYE"
+echo "$PERSON2 : Ok Bye Take care"
+
+
