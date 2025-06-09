d0d9de9604c692f323bd9b7cdddefb28c29e8f1b
siva <sivadevops4444@gmail.com>
2025-06-09 12:15:01 +0530

    started

---
+++ mysql-install.sh
@@ -0,0 +1,20 @@
+
+
+#this is the installation of MYSQL Package 
+
+#!/bin/bash
+
+#check the current running user is root ot not 
+
+USERID=$(id -u)
+
+if [ $USERID -eq 0 ]
+then
+echo " Great : You are the root user, so you can do install"
+else
+echo " Sorry : You are the not ROOT user, So you have no privilizes to do installation of MYSQL"
+fi  #fi means exit the script here
+
+
+
+
