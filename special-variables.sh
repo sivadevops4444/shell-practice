c8cdb577a11f93316f0778a8b29190c3bec3b833
siva <sivadevops4444@gmail.com>
2025-06-09 11:30:13 +0530

    started

---
+++ special-variables.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "All variables passed in to the script : $@"
+echo "Number of Variables : $#"
+echo "Script Name : $0"
+echo "Current working directory : $PWD"
+echo "Current user running this script : $USER"
+echo "Home directory of the user : $HOME"
+echo "PID of the script : $$"
+echo "PID of the last command in the background : $!"
+
+
+echo " SUCCESS : Above all the special variables with operators"
+
+
+echo "Thank you Siva for the information"
+
+echo " bye"
