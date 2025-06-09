f324ddab1245f8ae719622a223e6e966a4dde7d6
siva <sivadevops4444@gmail.com>
2025-06-09 23:02:19 +0530

    started

--- ownmysql.sh
+++ ownmysql.sh
@@ -3,9 +3,9 @@
 #!/bin/bash
 
 
-USERID=$1
+USERID=$(id -u)
 
-if [ $USERID - eq 0 ]
+if [ $USERID -eq 0 ]
     then 
     echo " HI, Great You are the ROOT user and you can install the package"
 
@@ -15,7 +15,7 @@ if [ $USERID - eq 0 ]
 
     yum install mysql -y
 
-    if [ $? - eq 0 ]
+    if [ $? -eq 0 ]
     then 
     echo " Hey HERO..You have installed MYSQL package successfully"
 
