26fd24889b327bb01b030938175ef556a86be357
siva <sivadevops4444@gmail.com>
2025-06-09 23:23:23 +0530

    started

--- ownmysql.sh
+++ ownmysql.sh
@@ -8,10 +8,10 @@ USERID=$(id -u)
 if [ $USERID -eq 0 ]
     then 
     echo " HI, Great You are the ROOT user and you can install the package"
-
     else
     echo " Hi Sorry...You are not a ROOT user. Please run with ROOT user credencials...Thank you"
     fi
+
     rpm -qa | grep -i mysql
 
     if [ $? -eq 0 ]
@@ -19,13 +19,13 @@ if [ $USERID -eq 0 ]
     echo " MYSQL Package is already installed...Thank you"
     else
     echo " MYSQL Package is not installed so...Installing NOW...."
+    fi
 
     yum install mysql -y
 
     if [ $? -eq 0 ]
     then 
     echo " Hey HERO..You have installed MYSQL package successfully"
-
     else
     echo " Hey buddy..Sorry MYSQL Package was not installed "
     fi
