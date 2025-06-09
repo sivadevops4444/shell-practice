de7af3bae094bc4f2f53d35979f0ab4b2ef663c3
siva <sivadevops4444@gmail.com>
2025-06-09 22:46:42 +0530

    started

---
+++ ownmysql.sh
@@ -0,0 +1,30 @@
+
+#This is the own writing script for the MYSQL package installation 
+
+
+#!/bin/bash
+
+
+ROOT_USERID=$1
+
+if [ $ROOT_USERID -eq 0 ]
+    
+    then 
+
+    echo " HI, Great You are the ROOT user and you can install the package"
+
+    else
+    echo " Hi Sorry...You are not a ROOT user. Please run with ROOT user credencials...Thank you"
+
+    yum install mysql -y
+
+    if [ $? -eq 0 ]
+    then 
+    echo " Hey HERO..You have installed MYSQL package successfully"
+
+    else
+    echo " Hey buddy..Sorry MYSQL Package was not installed "
+
+    fi
+
+    
