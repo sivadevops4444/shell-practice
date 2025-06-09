be63d8ef5f9da32303b4b5395ba393e6f3a172fe
siva <sivadevops4444@gmail.com>
2025-06-09 11:53:41 +0530

    started

---
+++ conditions.sh
@@ -0,0 +1,27 @@
+
+
+#!/bin/bash
+
+#   -gt     ----> Greater Than
+#   -lt     ----> Less Than
+#   -eq     ---->  Equal to
+#   -ne     ---->  Not equal to 
+
+
+#below is example program for conditions
+
+echo "Below is the example program for conditions"
+
+NUMBER=$1
+
+if (NUMBER -lt 10)
+
+then
+echo " you are givin number is the less than 10 "
+else
+echo " you are givin number is not less than 10 "
+
+fi
+
+
+
