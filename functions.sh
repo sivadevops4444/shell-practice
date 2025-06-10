01cdda62cf9b07411bd403d9343abfea52fdf2d4
siva <sivadevops4444@gmail.com>
2025-06-10 09:21:02 +0530

    started

--- functions.sh
+++ functions.sh
@@ -11,3 +11,13 @@ exit 1
 else
 echo " You are running with root access"
 fi
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo "Installing $2 is ... SUCCESS"
+    else
+        echo "Installing $2 is ... FAILURE"
+        exit 1
+    fi
+
