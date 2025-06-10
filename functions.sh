171f11b028328dda6961643b09434803534f3a97
siva <sivadevops4444@gmail.com>
2025-06-10 09:26:28 +0530

    started

--- functions.sh
+++ functions.sh
@@ -12,12 +12,13 @@ else
 echo " You are running with root access"
 fi
 
-VALIDATE(){
-    if [ $1 -eq 0 ]
-    then
-        echo "Installing $2 is ... SUCCESS"
-    else
-        echo "Installing $2 is ... FAILURE"
-        exit 1
-    fi
+yum install mysql
+
+if [ $? -eq 0 ]
+then
+    echo " You have successfully installed MYSQL"
+else
+    echo " Sorry.. MYSQL was not installed"
+fi
+
 
