84e50b689a19db3fd53bd0946f6df1c63664d320
siva <sivadevops4444@gmail.com>
2025-06-11 09:54:38 +0530

    started

---
+++ filefind.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Check if a file exists
+if [ -f "myfile.txt" ]; then
+  echo "File exists."
+  # Perform some operations on the file
+  exit 0  # Exit with success if file exists
+else
+  echo "File does not exist."
+  exit 1  # Exit with failure if file doesn't exist
+fi
