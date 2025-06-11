a1c5e9426832e077e71116bfa8fdd9ede870bd68
siva <sivadevops4444@gmail.com>
2025-06-11 10:05:07 +0530

    started

--- mysqlremoval.sh
+++ mysqlremoval.sh
@@ -3,9 +3,15 @@
 rpm -qa | grep -i mysql
 if [ $? -0 ]
 then
-    echo " MYSQL is installed so...Going to remove.
+    echo " MYSQL is installed so...Going to remove."
 else
     echo " MYSQL is already uninstalled so..Nothing to DO."
+fi
 
 yum remove mysql-y
+if [ $? -eq 0 ]
+then
+    echo "MYSQL Package has been removed"
+else
+    echo "MYSQL package not there to remove"
 fi
